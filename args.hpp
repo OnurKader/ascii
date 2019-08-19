d99efa0a0acb2ef951d88f4d0113a409983c4d77
OnurKader <onurorkunkader1999@gmail.com>
2019-08-19 14:49:18 +0300

    Added -c color and -i inverse options

--- args.hpp
+++ args.hpp
@@ -15,7 +15,7 @@ struct Option
 		double_dashed
 	} mode;
 
-	Option() : name("NaN"), mode(str) {}
+	Option() : name(""), mode(str) {}
 	explicit Option(const std::string &name) : name(name), mode(str) {}
 };
 
@@ -27,31 +27,20 @@ private:
 	const std::string name;
 
 public:
-	Args();
 	Args(int &, char **);
 	~Args();
-	void parseArgs(int &, char **);
 	std::vector<std::string> getArgs() const;
 	void parseOpt(const std::string &);
-	void parseOpts(const std::vector<std::string> &);
 	std::unordered_map<std::string, Option> getOpts() const;
-	Option getOpt(const std::string) const;
 	Option getOpt(const std::string &) const;
 	bool optExists(const std::string &) const;
 	std::string getName() const;
 	void convert();
 };
 
-#endif
-
-Args::Args() : name("How Can A Program Have No Name?!")
-{
-	arguments.reserve(4);
-}
 
 Args::Args(int &argc, char **argv) : name(std::string(argv[0]).substr(std::string(argv[0]).rfind('/') + 1))
 {
-	arguments.reserve(argc);
 	for (size_t i = 0; i < (unsigned)argc; ++i)
 		arguments.push_back(std::string(argv[i]));
 }
@@ -62,15 +51,9 @@ Args::~Args()
 	options.clear();
 }
 
-void Args::parseArgs(int &argc, char **argv)
-{
-	arguments.reserve(argc);
-	for (size_t i = 0; i < (unsigned)argc; ++i)
-		arguments.push_back(std::string(argv[i]));
-}
-
 std::vector<std::string> Args::getArgs() const { return arguments; }
 
+// Push the given option, --xxx | -x | x, into the options map
 void Args::parseOpt(const std::string &option)
 {
 	// Add --'color'=auto, now it's just --'color=auto'
@@ -78,44 +61,29 @@ void Args::parseOpt(const std::string &option)
 	{
 		Option *temp = new Option(option);
 		temp->mode = Option::double_dashed;
-		options.insert(std::make_pair(option.substr(2), *temp));
+		options.insert(std::make_pair(option, *temp));
 		delete temp;
 	}
 	else if (option.front() == '-')
-	{
-		// For every letter after -x add that option, for loop
-		for (unsigned char letter : option.substr(1))
+		// For every letter after -x add that option, -lah eg.
+		for (const char &letter : option.substr(1U))
 		{
-			std::string str;
-			str.assign(1, letter);
+			if (letter == ' ')
+				break;
+			std::string str("-");
+			str += letter;
 			Option *temp = new Option(str);
 			temp->mode = Option::dashed;
 			options.insert(std::make_pair(str, *temp));
 			delete temp;
 		}
-	}
 	else
-	{
-		Option *temp = new Option(option);
-		options.insert(std::make_pair(option, *temp));
-		delete temp;
-	}
-}
-
-void Args::parseOpts(const std::vector<std::string> &options)
-{
-	for (const std::string &option : options)
-	{
-		Option temp(option);
-		parseOpt(option);
-	}
+		options.insert(std::make_pair(option, *(new Option(option))));
 }
 
 std::unordered_map<std::string, Option> Args::getOpts() const { return options; }
 
-Option Args::getOpt(const std::string option) const { return options.at(option); }
-
-Option Args::getOpt(const std::string &option) const { return options.at(option); }
+Option Args::getOpt(const std::string &option) const { return optExists(option) ? options.at(option) : Option(); }
 
 bool Args::optExists(const std::string &option) const { return options.find(option) != options.end(); }
 
@@ -126,3 +94,5 @@ void Args::convert()
 	for (const std::string &temp : arguments)
 		parseOpt(temp);
 }
+
+#endif
--- img2ascii.cpp
+++ img2ascii.cpp
@@ -16,7 +16,7 @@ T map(T current_value, T minimum_domain_value, T maximum_domain_value, T minimum
 void getSize(unsigned short &width, unsigned short &height);
 
 // Get the Luminesence, human eyes see green better, use a formula from the interwebs
-int brightness(unsigned char &red, unsigned char &green, unsigned char &blue);
+int brightness(const unsigned char &red, const unsigned char &green, const unsigned char &blue);
 
 /// MAIN FUNCTION
 int main(int argc, char **argv)
@@ -33,30 +33,45 @@ int main(int argc, char **argv)
 
 	for (auto &item : arg_parser.getOpts())
 		if (item.second.mode == Option::str && item.second.name != argv[0U])
-		{
 			image_path = item.second.name;
-			break;
-		}
+	bool colorful = arg_parser.optExists("-c") || arg_parser.optExists("--color");
+	bool inverse = arg_parser.optExists("-i") || arg_parser.optExists("--inverse");
 
 	// ASCII Character list of 70 characters ranging from 0 Brightness to 255
-	const char symbols[] = "  .`\'^\",:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#&$8%BN@MW";
+	/* const char symbols[] = "  .`\'^\",:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#&$8%BN@MW"; */
+	// 102 chars
+	const char symbols[] = "  ..,,,'''``--_:;^^**""=+<>iv%&xclrs)/){}I?!][1taeo7zjLunT#@JCwfy325Fp6mqSghVd4EgXPGZbYkOA8U$KHDBWNMR0Q";
 
 	CImg<unsigned char> image(image_path.c_str());
 	const bool grey_scale = image.spectrum() == 1U;
 
 	const unsigned int new_height = term_height * 8U;
+	const unsigned int new_width = term_width * 4U;
 	const float image_aspect = (float)image._width / (float)image._height;
-	image.resize(new_height * image_aspect, new_height, -100, -100, 5);
+
+	if(image_aspect < 1.f)
+		image.resize(new_height * image_aspect, new_height);
+	else
+		image.resize(new_width, new_width/image_aspect);
+
 	for(unsigned int rows = 0U; rows <= image.height() - 8; rows += 8)
 	{
 		for(unsigned int cols = 0U; cols <= image.width() - 4; cols += 4)
 		{
-			unsigned char r = image._atXY(cols, rows, 0, grey_scale ? 0 : 0);
-			unsigned char g = image._atXY(cols, rows, 0, grey_scale ? 0 : 1);
-			unsigned char b = image._atXY(cols, rows, 0, grey_scale ? 0 : 2);
-			std::cout << "\033[38;2;" << (unsigned int)r << ';' << (unsigned int)g << ';' << (unsigned int)b << 'm' << symbols[map(brightness(r, g, b), 0, 255, 0, 69)] << "\033[m";
+			if(grey_scale || !colorful)
+			{
+				unsigned int b = image._atXY(cols, rows, 0, 0);
+				std::cout << symbols[map(b, 0U, 256U, 0U, 102U)];
+			}
+			else {
+				std::cout << "\033[38;2;0;0;0m";
+				uint8_t r = image._atXY(cols, rows, 0, 0);
+				uint8_t g = image._atXY(cols, rows, 0, 1);
+				uint8_t b = image._atXY(cols, rows, 0, 2);
+				std::cout << (inverse ? "\033[48;2;" : "\033[38;2;") << (uint16_t)r << ';' << (uint16_t)g << ';' << (uint16_t)b << 'm' << symbols[map(brightness(r, g, b), 0, 256, 0, 102)];
+			}
 		}
-		std::cout << std::endl;
+		std::cout << "\033[m" << std::endl;
 	}
 	return 0;
 } /// END OF MAIN
@@ -77,7 +92,7 @@ void getSize(unsigned short &width, unsigned short &height)
 	height = size.ws_row;
 }
 
-int brightness(unsigned char &red, unsigned char &green, unsigned char &blue)
+int brightness(const unsigned char &red, const unsigned char &green, const unsigned char &blue)
 {
 	return (int)(red * 0.22f + green * 0.71f + blue * 0.07f);
 }
